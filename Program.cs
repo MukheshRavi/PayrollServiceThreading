1f6d2bab6d24155a9eb6b6b34c8006fadd885bd9
MukheshRavi <mukeshparvathi@gmail.com>
2020-11-12 20:59:46 +0530

    [Mukhesh].Add UC1 Add Multiple Employee With Threading

--- Program.cs
+++ Program.cs
@@ -10,21 +10,47 @@ namespace PayrollServiceThreading
             Console.WriteLine("Hello World!");
             List<EmployeeDetails> employeelist = new List<EmployeeDetails>();
             PayrollRepository payrollRepository = new PayrollRepository();
-            employeelist.Add(new EmployeeDetails {empName="Vijay",gender='M',phoneNumber="2345167868",startDate= new System.DateTime(2019, 08, 24),
-               payrollId=4, BasePay=36000,Deductions=1000,IncomeTax=2000,TaxablePay=4000});
+            //employeelist.Add(new EmployeeDetails {empName="Vijay",gender='M',phoneNumber="2345167868",startDate= new System.DateTime(2019, 08, 24),
+            //   payrollId=4, BasePay=36000,Deductions=1000,IncomeTax=2000,TaxablePay=4000});
+            //employeelist.Add(new EmployeeDetails
+            //{
+            //    empName = "Sankar",
+            //    gender = 'M',
+            //    phoneNumber = "2345167456",
+            //    startDate = new System.DateTime(2019, 11, 09),
+            //    payrollId=5,
+            //    BasePay = 40000,
+            //    Deductions = 1500,
+            //    IncomeTax = 2000,
+            //    TaxablePay = 4000
+            //});
+            //payrollRepository.AddMultipleEmployee(employeelist);
             employeelist.Add(new EmployeeDetails
             {
-                empName = "Sankar",
+                empName = "Manish",
                 gender = 'M',
-                phoneNumber = "2345167456",
+                phoneNumber = "2345167898",
+                startDate = new System.DateTime(2019, 08, 24),
+                payrollId = 6,
+                BasePay = 80000,
+                Deductions = 1000,
+                IncomeTax = 4000,
+                TaxablePay = 5000
+            });
+            employeelist.Add(new EmployeeDetails
+            {
+                empName = "Smriti",
+                gender = 'F',
+                phoneNumber = "2345167437",
                 startDate = new System.DateTime(2019, 11, 09),
-                payrollId=5,
+                payrollId = 7,
                 BasePay = 40000,
                 Deductions = 1500,
                 IncomeTax = 2000,
                 TaxablePay = 4000
             });
-            payrollRepository.AddMultipleEmployee(employeelist);
+
+            bool actual = payrollRepository.AddMultipleEmployeeWithThreads(employeelist);
         }
     }
 }
